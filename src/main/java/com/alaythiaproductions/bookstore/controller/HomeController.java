f59908592ee1461aabc4473d2c3cf236feef538c
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-01-18 14:11:47 -0600

    Front-End Homepage and My Account

---
+++ src/main/java/com/alaythiaproductions/bookstore/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.alaythiaproductions.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/myAccount")
+    public String myAccount() {
+        return "myAccount";
+    }
+
+}
